733ab73485b44169c8e4803d963cfb30cd852494
KevinM55 <mduduzikevin93@gmail.com>
2024-10-31 13:40:59 +0200

    Added more dialogue between user and the bot

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ function toggleChatbot() {
     }
 } 
  const keywords = ["name","hello","hi","greetings","hey", "education", "project", "skills", "soft","summary",
-    "technical", "about","github","linkedin","yes","contact","socials","social","location", "contact", "Timeline", "programming language"]
+    "technical", "about","github","linkedin","yes","contacts","collision","detection","vehicle","shape","iot ",
+    "socials","social","location", "contact","collision", "timeline", "programming", "ar", "language",
+"restaurant","menu","plc", "water", "level", "control","iot","certificate","certification","shape","recognition"]
     let sentence = " ";
 function findKeyword(userInput, keywords){
     for (let keyword of keywords){if(userInput.includes(keyword)) return keyword;}
@@ -55,17 +57,32 @@ function sendMessage() {
             + "LinkedIn: " 
             + "GitHub:";
                 break;
-            case "project":
-            case "projects":
-
+            case "project":  case "projects":
                 response = "I’ve worked on various projects that showcase my technical expertise"
                 + "and problem-solving abilities. Here are a few examples:" + 
                 "Project list: "
                  +" 1. Collision Detection Vehicle"
                  +"2. AR Restaurant Menu"
                  +"3. PLC Water Level Control"
-                 +"4. ";
+                 +"4. Shape Recognition" + "Let me know if you would like to more...";
                 break;
+                case "collision": case "detection": case "vehicle":
+                 response = "This project focuses on using sensors to detect and avoid obstacles"
+                 +" autonomously. Technologies used include Arduino and L298 motor driver"
+                  +"including Ultrasonic and IR Sensors "; 
+                  break;
+                case "ar": case "restaurant": case "menu":
+                response = "An interactive menu system using augmented reality. Developed using Unity,"+
+                "Blender, ARkit and ARcore for seamless food ordering process";
+                break;
+                case "plc": case "water": case "level": case "control":
+                    response = "This project is designed to monitor and control water levels using a Programmable"
+                    + "logic controller. It simulates an automated process in which the PLC manages water inflow and outflow";
+                    break;
+                case "iot": case "shape": case "recognition":
+                    response = "This project showcases the use of IOT technology for shape recognition," + 
+                    "utilizing a Rasperi Pi as the primary processing";
+                    break;
             case "skills":
                 response = "I possess a variety of technical and soft skills of which include"
                 +"Web development, programming, ML, critical thinking and time management";
@@ -74,11 +91,18 @@ function sendMessage() {
                 response = "I recently graduated with a National Diploma in Information Technology" + 
                 " (Intelligent Industrial Systems) from Tshwane University of Technology. "+
                 "I’m passionate about creating solutions in intelligent systems and IoT. Would you like to learn about my skills or my projects?";
-                if(userInput ==="yes"){response = "My projects";}
+              //  if(userInput ==="yes"){response = "My projects";}
                 break;
+            case "education": 
+            response = "N.Dip IT (Intelligent Industrial Sytems) "+ "Tshwane University of Technology"
+            + "Relevant courses: "
+            + "Industrial Control Systems\t Robotics & Automation\t Embedded systems\t" 
+            + "Machine Learning\t IoT Applications ";
+            break;  
             default:
-             response = "I'm here to assist you!";
+         response = "I'm here to assist you!";
                 break; }
+             
     }
     addMessage(`Chatbot: ${response}`, "bot-message");
     document.getElementById("user-input").value = "";
